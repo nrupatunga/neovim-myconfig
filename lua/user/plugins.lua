2efca56fb6b70d263d960e6df2ea03e46a9b6af4
nrupatunga <nrupatunga.tunga@gmail.com>
2022-06-30 12:04:12 +0530

    update with null ls and lsp

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -4,7 +4,7 @@ vim.call('plug#begin', '~/.config/nvim/plugged')
 
 --these are my old vim plugins I still use
 Plug 'christoomey/vim-tmux-navigator' -- switching between panes with tmux + vim
-Plug 'dense-analysis/ale'
+--Plug 'dense-analysis/ale'
 Plug 'jiangmiao/auto-pairs' -- pair the braces
 Plug 'liuchengxu/vim-which-key' -- this is helpful when I want to find what leader key is mapped too
 Plug 'mhinz/vim-startify' -- startify (im used to it)
@@ -24,9 +24,15 @@ Plug 'hrsh7th/nvim-cmp'
 Plug 'hrsh7th/cmp-buffer'
 Plug 'hrsh7th/cmp-path'
 Plug 'saadparwaiz1/cmp_luasnip' -- snippet completion
+Plug 'hrsh7th/cmp-nvim-lsp'
 
 -- luasnip for completion
 Plug 'L3MON4D3/LuaSnip'
 Plug 'rafamadriz/friendly-snippets'
+
+-- lsp
+Plug 'neovim/nvim-lspconfig'
+Plug 'williamboman/nvim-lsp-installer'
+Plug 'jose-elias-alvarez/null-ls.nvim'
 
 vim.call('plug#end')
