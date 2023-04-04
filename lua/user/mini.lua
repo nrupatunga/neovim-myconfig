22634aef9ee2fe7283e1130afe9bfe7ff7d53096
nrupatunga <nrupatunga.tunga@gmail.com>
2023-04-04 22:45:29 +0530

    buggy tabline in mini

--- lua/user/mini.lua
+++ lua/user/mini.lua
@@ -65,16 +65,16 @@ require("mini.basics").setup({
 	silent = false,
 })
 
-require("mini.tabline").setup({
-	-- Whether to show file icons (requires 'nvim-tree/nvim-web-devicons')
-	show_icons = false,
-	-- Whether to set Vim's settings for tabline (make it always shown and
-	-- allow hidden buffers)
-	set_vim_settings = true,
-	-- Where to show tabpage section in case of multiple vim tabpages.
-	-- One of 'left', 'right', 'none'.
-	tabpage_section = "none",
-})
+--require("mini.tabline").setup({
+	---- Whether to show file icons (requires 'nvim-tree/nvim-web-devicons')
+	--show_icons = false,
+	---- Whether to set Vim's settings for tabline (make it always shown and
+	---- allow hidden buffers)
+	--set_vim_settings = true,
+	---- Where to show tabpage section in case of multiple vim tabpages.
+	---- One of 'left', 'right', 'none'.
+	--tabpage_section = "none",
+--})
 
 require("mini.move").setup( -- No need to copy this inside `setup()`. Will be used automatically.
 	{
