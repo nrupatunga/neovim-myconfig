4566744016d8375ff7c9012df447d7021485f190
nrupatunga <nrupatunga.tunga@gmail.com>
2023-04-04 16:22:40 +0530

    update cmp.lua

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -118,16 +118,16 @@ cmp.setup({
 		}),
 	},
 	formatting = {
-		--fields = { "kind", "abbr", "menu" },
-		fields = { "abbr", "menu" },
+		fields = { "kind", "abbr", "menu" },
+		--fields = { "abbr", "menu" },
 		format = function(entry, vim_item)
 			-- Kind icons
-			vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-			-- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+			--vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+			vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
 			vim_item.menu = ({
 				nvim_lsp = "[LSP]",
-				luasnip = "[Snippet]",
-				buffer = "[Buffer]",
+				luasnip = "[Snip]",
+				buffer = "[Buf]",
 				path = "[Path]",
 			})[entry.source.name]
 			return vim_item
